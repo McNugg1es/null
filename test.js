2356e3b502ae0b391562dfcb7923432ec2c9760a
William Johansson <william.johansson5@learnet.se>
2022-08-26 15:45:29 +0200

    lektion3

---
+++ test.js
@@ -0,0 +1,35 @@
+/*
+console.log("Hej")
+console.log(1+2)
+
+var UserName = "McNuggies"
+
+console.log(UserName)
+
+UserName = "Stefan"
+
+console.log(UserName)
+
+*/
+
+/*
+//gg
+console.log("ris")
+console.log("ingles")
+
+var ps = require("prompt-sync");
+var prompt = ps();
+
+let name = prompt ("Enter your name:");
+// Write your code here:
+
+console.log(name)
+*/
+
+var ps = require("prompt-sync");
+var prompt = ps();
+
+let name = prompt ("Enter Hello:");
+console.log(name)
+console.log(name)
+console.log(name)
