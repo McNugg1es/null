6ac4c3f3ea838fd40d0f1161054bf5e537fd0a72
William Johansson <william.johansson5@learnet.se>
2022-08-29 13:05:08 +0200

    balls

--- test.js
+++ test.js
@@ -25,7 +25,7 @@ let name = prompt ("Enter your name:");
 
 console.log(name)
 */
-
+/*
 var ps = require("prompt-sync");
 var prompt = ps();
 
@@ -33,3 +33,22 @@ let name = prompt ("Enter Hello:");
 console.log(name)
 console.log(name)
 console.log(name)
+*/
+/*
+var ps = require("prompt-sync");
+var prompt = ps();
+
+let name = prompt ("What is your name?");
+console.log ("Hello " + name +"!") 
+*/
+
+var ps = require("prompt-sync");
+var prompt = ps();
+
+let name = prompt ("Hello, how are you?" );
+
+let name2 = prompt ("That's interesting, tell me more."
+);
+
+console.log ("Thank you for sharing!")
+
