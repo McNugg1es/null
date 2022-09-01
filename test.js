b337040c6467a65d9167d050665e227e5579f801
William Johansson <william.johansson5@learnet.se>
2022-09-01 11:05:41 +0200

    lol

--- test.js
+++ test.js
@@ -12,6 +12,7 @@ console.log(UserName)
 
 */
 
+
 /*
 //gg
 console.log("ris")
@@ -25,6 +26,8 @@ let name = prompt ("Enter your name:");
 
 console.log(name)
 */
+
+
 /*
 var ps = require("prompt-sync");
 var prompt = ps();
@@ -34,6 +37,8 @@ console.log(name)
 console.log(name)
 console.log(name)
 */
+
+
 /*
 var ps = require("prompt-sync");
 var prompt = ps();
@@ -42,13 +47,36 @@ let name = prompt ("What is your name?");
 console.log ("Hello " + name +"!") 
 */
 
+
+/*
 var ps = require("prompt-sync");
 var prompt = ps();
 
-let name = prompt ("Hello, how are you?" );
+let q1 = prompt ("Hello, how are you?") 
 
-let name2 = prompt ("That's interesting, tell me more."
-);
+if ((q1 == "Fine, thanks") || (q1 == "fine, thanks")) { 
+    
+let q2 = prompt ("That's interesting, tell me more: ") 
 
-console.log ("Thank you for sharing!")
+if ((q2 == "I learn coding") || (q2 == "i learn coding"))
+
+console.log ("Thank you for sharing") 
+}
+*/
+
+
+/*
+var ps = require("prompt-sync");
+var prompt = ps();
+
+console.log ("I will tell a story, but I need some information.")
+let name = prompt ("Give a name for main character:")
+console.log (name) 
+
+let profession = prompt ("Give the character a profession:") 
+console.log (profession)
+
+console.log ("Here is the story: \nOnce upon a time there was a Data Scientist called Ada. On her way to work, Ada often pondered what being Data Scientist meant to them. When you work as a Data Scientist you meet interesting people. Ada enjoys their work as Data Scientist, \nThe end.") 
+*/
+
 
