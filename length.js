4d397f0606c367502efb239eb21696e78e3029d7
McNugg1es <willejohansson3810@gmail.com>
2022-10-13 19:08:13 +0200

    ellå

---
+++ length.js
@@ -0,0 +1,7 @@
+const clothing = ['shoes', 'shirts', 'socks', 'sweaters'];
+
+console.log(clothing.length);
+// expected output: 4
+
+// Length-egenskapen för ett objekt som är en instans av typen Array ställer in eller returnerar antalet element i den arrayen. 
+// Värdet är ett 32-bitars heltal utan tecken som alltid är numeriskt större än det högsta indexet i arrayen.
